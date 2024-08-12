82ee6398d00785097504f94a56c699f3e440104c
thereterry <139209658+thereterry@users.noreply.github.com>
2024-08-12 10:45:00 +0200

    hero.video

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -8,7 +8,6 @@ import hotStoneImg from '../../assets/Billeder/extra_procedures_etc/5.jpg';
 import waxingImg from '../../assets/Billeder/extra_procedures_etc/6.jpg';
 import facialImg from '../../assets/Billeder/extra_procedures_etc/7.jpg';
 import Icon from '../../assets/Billeder/icons/1.png'
- import { NavLink } from 'react-router-dom';
 
 
 const About = () => {
@@ -53,37 +52,13 @@ const About = () => {
     `,
   }}
 ></p>
-{/* 
-        <button className="px-10 py-3 bg-[#F26A6C] text-white text-lg uppercase rounded-full hover:text-black">Read More</button>
-      </div>
-      <img src={flowerRight} alt="Flower" className="absolute right-10 top-40 w-24 h-24" />
-      <div className="container mx-auto px-4 mt-10">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {[massageImg, hotStoneImg, waxingImg, facialImg].map((img, index) => (
-            <div key={index} className="relative group">
-              <img src={img} alt="Procedure" className="w-full h-auto" />
-              <div className="absolute inset-0  bg-[#F26A6C] bg-opacity-75 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <img src={Icon} alt="Icon" className="text-white text-4xl mb-2" />
-                <p className="text-white  font-display">Body Treatment</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    
-    
 
-    </section> */}
      <button 
           onClick={toggleReadMore} 
           className="px-10 py-3 bg-[#F26A6C] text-white text-lg uppercase rounded-full hover:text-black">
           {isExpanded ? 'Show Less' : 'Read More'}
         </button>
-        {/* <div className="mt-6">
-            <NavLink to="/feature" className="text-lg text-blue-500 hover:text-blue-700">
-                Learn more about our Features
-          </NavLink>
-        </div> */}
+     
       </div>
       <img src={flowerRight} alt="Flower" className="absolute right-10 top-40 w-24 h-24" />
       <div className="container mx-auto px-4 mt-10">
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -9,33 +9,19 @@ const Hero = () => {
 
   const [heroData, setHeroData] = useState(null);
 
-  const [isVideoVisible, setIsVideoVisible] = useState(false); 
-
   useEffect(() => {
     makeRequest('http://localhost:5029/hero');
   }, [makeRequest]);
 
   useEffect(() => {
     if (data) {
-      // Filter the data to find the one with "show": true
       const filteredData = data.find(item => item.show === true);
       setHeroData(filteredData);
     }
   }, [data]);
 
-  const handlePlayClick = () => {
-    setIsVideoVisible(true);
-  };
-
-  const handleCloseClick = () => {
-    setIsVideoVisible(false);
-  };
-
-  // Show loading or error state if needed
   if (isLoading) return <p>Loading...</p>;
-
   if (error) return <p>Error loading data</p>;
-
   if (!heroData) return <p>No data available</p>;
 
   return (
@@ -43,15 +29,13 @@ const Hero = () => {
       <img src={leafImage} alt="Leaf" className="absolute left-0 top-15" />
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex-1 ml-20">
-        <h2 className="text-xl font-medium text-red-400 mb-10 uppercase">
-          {heroData.title1.replace("Beuty", "Beauty")}
-        </h2>
-          {/* <h2 className="text-xl font-medium text-red-400 mb-10 uppercase">{heroData.title1}</h2> */}
+          <h2 className="text-xl font-medium text-red-400 mb-10 uppercase">
+            {heroData.title1.replace("Beuty", "Beauty")}
+          </h2>
           <h1 className="text-6xl mb-4">
             {heroData.title2.split(' ').slice(0, 3).join(' ')} <br />
             {heroData.title2.split(' ').slice(3).join(' ')}.
           </h1>
-          
           <p
             className="text-lg mb-6 text-gray-500 font-thin"
             dangerouslySetInnerHTML={{ __html: heroData.content }}
@@ -60,35 +44,18 @@ const Hero = () => {
             <button className="px-6 py-3 bg-[#F26A6C] text-white text-lg hover:text-black">
               RESERVE NOW
             </button>
-            <div
-              onClick={handlePlayClick}
-              className="flex items-center space-x-2 text-lg text-gray-700 hover:text-gray-900 cursor-pointer"
+            <a
+              href={`https://www.youtube.com/watch?v=${heroData.link.split('?v=')[1]}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-2 text-lg text-gray-700 hover:text-gray-900"
             >
               <div className="flex items-center justify-center w-10 h-10 bg-red-50 rounded-full">
                 <CiPlay1 className="text-pink-400" style={{ fontSize: '0.80rem' }} />
               </div>
               <span>Watch our story</span>
-            </div>
+            </a>
           </div>
-          {isVideoVisible && (
-            <div className="relative mt-4 w-full flex justify-center">
-              <button
-                onClick={handleCloseClick}
-                className="absolute top-0 right-0 bg-red-500 text-white rounded-full p-2 z-20"
-              >
-                X
-              </button>
-              <iframe
-                width="960"
-                height="540"
-                src={`https://www.youtube.com/embed/QWUPm0ND7HY?autoplay=1`} // Use the embed URL format
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-          )}
         </div>
         <div className="flex-1 text-right hidden md:block">
           <img
